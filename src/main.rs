034099d70f6476ab0fddb4349840c6e1a71d720f
AchroDev <achrodev@proton.me>
2024-06-15 15:21:28 -0500

    fixed line spacing within first paragraph

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 /*
 *   A struct, or structure, is a custom data type that lets you package together and name
-*   multiple related values that make up a meaningful group. If you're familiar with an object-oriented language, a struct is like an object's data attributes.
-*   In this chapter, we'll compare and contrast tuples with structs to build on what you already
-*   know and demonstrate when structs are a better way to group data.
+*   multiple related values that make up a meaningful group. If you're familiar with an object-oriented language, a struct
+*   is like an object's data attributes. In this chapter, we'll compare and contrast tuples with structs to build on what
+*   you already know and demonstrate when structs are a better way to group data.
 *
 *   We'll demonstrate how to define and instantiate structs. We'll discuss how to define associated functions,
 *   epsecially the kind of associated functions called methods, to specify behavior associated with a struct type.
