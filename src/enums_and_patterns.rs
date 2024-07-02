403787b0c68d7af4e1a815bb64c4e42d89451c30
AchroDev <achrodev@proton.me>
2024-07-02 03:20:31 -0500

    Matches are Exhaustive

--- src/enums_and_patterns.rs
+++ src/enums_and_patterns.rs
@@ -322,3 +322,215 @@ let sum = x + y;
 *   construct that does just this when used with enums: it will run different code depending on which variant of the enum it has, and
 *   that code can use the data inside the matching value.
 */
+
+/*
+    The match Control Flow Contstruct
+*/
+
+/*
+*   Rust has an extremely powerful control flow contrust called 'match' that allows you to compare a value against a sereies
+*   of paterns and then execute code based on which pattern matches. Patterns can be made up of literal values, variable names, wildcards, and
+*   many other things; Chapter 18 covers all the different kinds of patterns and what they do. The power of 'match' comes from the
+*   expressiveness of the patterns and the fact that the compiler confirms that all possible cases are handled.
+*   
+*   Think of a 'match' expression as being like a coin-sorting machine: coins slide down a track with variously sized holes along it, and each
+*   coin falls though the first hole it encounters that it fits into. In the same way, values go through each pattern in a 'match', and at the first
+*   pattern value "fits", the value falls into the associated code block to be used during execution.
+*   
+*   Speaking of coins, let's use them as an example using 'match'! We can write a function that takes an unknown US coin and, in a similar way
+*   as the counting machine, determines which coin it is and returns its value in cents, as shown below:
+*/
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+/*
+*   Let's break down the 'match' in the 'value_in_cents' function. First we list the 'match' keyword followed by an expression, which
+*   in this case is the value 'coin'. This seems very similar to a conditional expression used with 'if', but there's a big difference:
+*   with 'if', the condition needs to evaluate to a Boolean value, but here it can be any type. The type of 'coin' in this example is the
+*   'Coin' enum that we defined on the first line.
+*   
+*   Next are the 'match' arms. An arm has two parts: a pattern and some code. The first arm here has a pattern that is the value 'Coin::Penny'
+*   and then the '=>' operator that separates the pattern and the code to run. The code in this case is just the value '1'. Each arm is separated
+*   from the next with a comma.
+*   
+*   When the 'match' expression executes, it compares the resultant value against the pattern of each arm, in order. If a pattern matches the value,
+*   the code associated with that pattern is executed. If that pattern doesn't match the value, execution continues to the next arm, much as in coin-sorting
+*   machine. We can have as many arms as we need: in the previous code, our 'match' has four arms.
+*   
+*   The code associated with each arm is an expression, and the resultant value of the expression in the matching arm is the value
+*   that gets retuned for the entire 'match' expression.
+*   
+*   We don't typically use curly brackets if the match arm code is short, as it is in the previous code where each arm just returns a value.
+*   If you want to run multiple lines of code in a match arm, you must use curly brackets, and the comma following the arm is then optional.
+*   For example, the following code prints "Lucky penny!" every time the method is called with a 'Coin::Penny', but still returns the last value
+*   of the block, '1':
+*/
+
+fn value_in_cents2(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky Penny!");
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+/*
+    Patterns That Bind to Values
+*/
+
+/*
+*   Another useful feature of match arms is that they can bind to the parts of the values that match the pattern. This is how we can
+*   extract values out of enum variants.
+*   
+*   As an example, let's change one of our enum variants to hold data inside it. From 1999 through 2008, the Unites States minted quarters
+*   with different designs for each of the 50 states on one side. No other coins got state designs, so only quarters have this extra value.
+*   We can add this information to our 'enum' by changing the 'Quarter' variant to include a 'UsState' value stored insid it, which
+*   is being done in the code below:
+*/
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+/*
+*   Let's imagine that a friend is trying to collect all 50 state quarters. While we sort our loose change by coin type, we'll also call out
+*   the name of the state associated with each quarter so that if it's one our friend doesn't have, they can add it to their collection.
+*   
+*   In the match expression for this code, we add a variable called 'state' to the pattern that matches values of the variant 'Coin::Quarter'.
+*   When a 'Coin::Quarter' matches, the 'state' variable will bind to the value of that quarter's state. Then we can use 'state' in
+*   the code for that arm, like so:
+*/
+
+fn value_in_cents3(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}", state);
+            25
+        }
+    }
+}
+
+/*
+*   If we were to call 'value_in_cents(Coin::Quarter(UsState::Alaska))', 'coin' would be 'Coin::Quarter(UsState::Alaska)'. When we
+*   compare that value with each of the match arms, none of them until we reach 'Coin::Quarter(state)'. At that point, the binding
+*   for 'state' will be the value 'UsState::Alaska'. We can then use that binding in the 'println!' expression, thus getting the inner
+*   state value out of the 'Coin' enum variant for 'Quarter'.
+*/
+
+/*
+    Matching with Option<T>
+*/
+
+/*
+*   In the previous sectionm, we wanted to get the inner 'T' value out of the 'Some' case when using 'Option<T>'; we can also handle
+*   'Option<T>' using 'match', as we did with the 'Coin' enum! Instead of comparing coins, we'll compare the variants of 'Option<T>, but
+*   the way 'match' expression works remains the same.
+*   
+*   Let's say we want to write a function that takes an 'Option<i32>' and, if there's a value inside, adds 1 to that value. If there isn't
+*   a value inside, the function should return the 'None' value and not attempt to perform any operations.
+*   
+*   This function is very easy to write, thanks to 'match', and will look like below:
+*/
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+}
+
+/*
+*   Let's examine the first execution of 'plus_one' in more detail. When we call 'plus_one(five)', the variable 'x' in the
+*   body of 'plus_one' will have the value 'Some(5)'. We then compare that against each match arm:
+*/
+
+None => None,
+
+/*
+*   The 'Some(5)' value doesn't match the pattern 'None', so we continue to the next arm:
+*/
+
+Some(i) => Some(i + 1),
+
+/*
+*   Does 'Some(5)' match 'Some(i)'? It does! We have the same variant. The 'i' binds to the value contained in 'Some', so 'i' takes
+*   the value '5'. The code in the match arm is then executed, so we add 1 to the value of 'i' and create a new 'Some' value with our
+*   total '6' inside.
+*   
+*   Now let's consider the second call of 'plus_one' in the code above, where 'x' is 'None'. We enter the 'match' and compare the first arm:
+*/
+
+None => None,
+
+/*
+*   It matches! There's no value to add to, so the program stops and returns the 'None' value on the right side of '=>'. Because the first
+*   arm matched, no other arms are compared.
+*   
+*   Combining 'match' and enums is useful in many situations. You'll see this pattern a lot in Rust code: 'match' against an enum, bind
+*   a variable to the data inside, and then execute code based on it. It's a bit tricky at first, but once you get used to it, you'll wish
+*   you had it in all languages. It's consistently a user favorite.
+*/
+
+/*
+    Matches are Exhaustive
+*/
+
+/*
+*   There's one other aspect of 'match' we need to discuss: the arms' patterns must cover all possibilities. Consider this version of our
+*   'plus_one' function, which has a bug and won't compile:
+*/
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        Some(i) => Some(i + 1),
+    }
+}
+
+/*
+*   We didn't handle the 'None' case, so this code will cause a bug. Luckily, it's a bug Rust knows how to catch. If we try to compile
+*   this code, we'll get this error:
+*   
+*   error[e0004]: non-exhaustive patterns: 'None' not covered (see the rest in the book or compiler)
+*/
+
+/*
+*   Rust knows that we didn't cover every possible case, and even knows which pattern we forgot! Matches in Rust are exhaustive: we must
+*   exhaust every last possibility in order for the code to be valid. Especially in the case of 'Option<T>', when Rust
+*   prevents us from forgetting to explicitly handle the 'None' case, it protects us from assuming that we have a value when we might have
+*   null, thus making the billion-dollar mistake discussed earlier impossible.
+*/
+
